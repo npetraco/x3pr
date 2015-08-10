e3bab0652ace6a01c08319527169b6671b7e44b6
npetraco <npetraco@gmail.com>
2015-08-10 15:37:44 -0400

    Added inner circle cropping to crop.circle.R

--- R/crop.circle.R
+++ R/crop.circle.R
@@ -12,11 +12,16 @@
 #'
 #' @examples XXXX
 #--------------------------------------------
-crop.circle <- function(x3p.surf.file.info, radius) {
+crop.circle <- function(x3p.surf.file.info, radius, type="outer", center.pt=NULL) {
   
   head.info <- x3p.surf.file.info[[1]]
   
-  centerb <- c(round(head.info$num.lines/2), round(head.info$num.pts.line/2))
+  if(is.null(center.pt)){
+    centerb <- c(round(head.info$num.lines/2), round(head.info$num.pts.line/2))
+  } else {
+    centerb <- center.pt #PUT IN A TRAP IF NOT TWO DIMENSIONAL
+  }
+  
   xinc <- as.numeric(head.info["x.inc"]) #should be microns already
   rb <- round(radius/xinc)
   
@@ -29,30 +34,58 @@ crop.circle <- function(x3p.surf.file.info, radius) {
   #Determine circular region to crop out in terms of matrix indices (pixels):
   circ.idxs <- circle.idxs(radius=radius, xc=0, yc=0, top.halfQ=TRUE)
   
-  #Initalize cropped surface container:
-  cropped.x3p.surf.file.info <- x3p.surf.file.info
-  cropped.surface <- array(NaN, dim(x3p.surf.file.info[[2]]) )
   
-  #Crop out the circular region:
-  for(i in 1:nrow(circ.idxs)) {
-    col.idx <- circ.idxs[i,1] + centerb[1]
+  #Crop out the OUTER circular region:
+  if(type=="outer") {
     
-    top.row.idx <- circ.idxs[i,2]
-    bot.row.idx <- -1*top.row.idx
+    #Initalize cropped surface container:
+    cropped.x3p.surf.file.info <- x3p.surf.file.info
+    cropped.surface <- array(NaN, dim(x3p.surf.file.info[[2]]) )
     
-    top.row.idx <- top.row.idx + centerb[2]
-    bot.row.idx <- bot.row.idx + centerb[2]
+    for(i in 1:nrow(circ.idxs)) {
+      col.idx <- circ.idxs[i,1] + centerb[1]
+      
+      top.row.idx <- circ.idxs[i,2]
+      bot.row.idx <- -1*top.row.idx
+      
+      top.row.idx <- top.row.idx + centerb[2]
+      bot.row.idx <- bot.row.idx + centerb[2]
+      
+      cropped.surface[top.row.idx:bot.row.idx, col.idx] <- x3p.surf.file.info[[2]][top.row.idx:bot.row.idx, col.idx]
+      #print(paste("Column:", col.idx, " Get row:", top.row.idx, "to row:", bot.row.idx))    
+    }
     
-    cropped.surface[top.row.idx:bot.row.idx, col.idx] <- x3p.surf.file.info[[2]][top.row.idx:bot.row.idx, col.idx]
-    #print(paste("Column:", col.idx, " Get row:", top.row.idx, "to row:", bot.row.idx))    
+  } else if(type=="inner") { #Crop out the INNER circular region:
+    
+    #Initalize cropped surface container:
+    cropped.x3p.surf.file.info <- x3p.surf.file.info
+    cropped.surface <- cropped.x3p.surf.file.info[[2]]
+    
+    for(i in 1:nrow(circ.idxs)) {
+      col.idx <- circ.idxs[i,1] + centerb[1]
+      
+      top.row.idx <- circ.idxs[i,2]
+      bot.row.idx <- -1*top.row.idx
+      
+      top.row.idx <- top.row.idx + centerb[2]
+      bot.row.idx <- bot.row.idx + centerb[2]
+      
+      cropped.surface[top.row.idx:bot.row.idx, col.idx] <- NaN
+      #print(paste("Column:", col.idx, " Get row:", top.row.idx, "to row:", bot.row.idx))    
+    }
+    
+  } else {
+    stop("Pick outer or inner region of the circle to crop.")
   }
   
   cropped.x3p.surf.file.info[[1]]$num.pts.line <- ncol(cropped.surface)
   cropped.x3p.surf.file.info[[1]]$num.lines <- nrow(cropped.surface)
   cropped.x3p.surf.file.info[[2]] <- cropped.surface
   
-  #Cut NaN borarder excess
-  cropped.x3p.surf.file.info <- crop.retangle(cropped.x3p.surf.file.info, percent.off.each.side=0)
+  #Cut NaN borarder excess for outer circle crop
+  if(type=="outer") {
+    cropped.x3p.surf.file.info <- crop.retangle(cropped.x3p.surf.file.info, percent.off.each.side=0)
+  }
   
   return(cropped.x3p.surf.file.info)
   
